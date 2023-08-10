581189c6ba64befe280500872caf0fc6f44a834f
GeralBot <botmateo67@gmail.com>
2023-08-09 20:08:03 -0500

    Mateo Bot

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,13 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `@𝐦𝐚𝐭𝐞𝐨_𝐯𝐞𝐧𝐭𝐚𝐬_: ${pesan}`
-let teks = `Creador Wa.me/51929741688 SI QUIERES BOT COMUNICAME
-
-☘️𝐌𝐚𝐭𝐞𝐨 𝐋𝐞𝐬 𝐏𝐢𝐝𝐞 𝐐𝐮𝐞 𝐃𝐞𝐬𝐩𝐢𝐞𝐫𝐭𝐞𝐧☘️ \n\n❏ ${oi}\n❏ 🫶🏻 ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:: \n`
+let oi = `𝙼𝙴𝙽𝚂𝙰𝙹𝙴:: ${pesan}`
+let teks = `⺀𝙰𝙽𝙾𝚃𝙰𝚃𝙴 𝙾 𝚂𝙾𝚂 𝙲𝚄𝙿𝙾⺀ \n\n❏ ${oi}\n❏ 🫶🏻 ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:: \n`
 for (let mem of participants) {
-teks += `❤️💙❖≽  @${mem.id.split('@')[0]}\n`}
-teks += `🐾𝐌𝐚𝐭𝐞𝐨 𝐁𝐨𝐭❤️💙`
+teks += `☁️@${mem.id.split('@')[0]}\n`}
+teks += `🐾𝗦𝗵𝗮𝘇𝘆 𝗕𝗼𝘁`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
